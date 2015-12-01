edccb905f04750f86ec476eb12e534f3d76f1de6
Marco Tejedor González <m.tejedorg@alumnos.urjc.es>
2015-12-01 14:54:31 +0100

    First taste of the shape

--- atoi_1.asm
+++ atoi_1.asm
@@ -1,6 +1,7 @@
 .data
 	menen:	.asciiz	"Escriba una frase\n"
 	buffer:	.space	1024
+	num:		.word
 	saltolinea:.asciiz	"\n"
 	
 .text
@@ -17,14 +18,20 @@ main:
 		syscall
 		
 		la $t0, buffer
-		la $t1, buffer
 		
-loop:		lb $t2, 0($t1)
-		beq $t2, 10, cont		#If LF, continues the program
-		addiu $t1, $t1, 1
+loop:		lb $t1, 0($t0)
+		mulu num, num, 10
+		subu $t2, $t1, '0'
+		addu num, num, $t2
+		beq $t1, 10, cont		#If LF, ends the program
+		addiu $t0, $t0, 1
 		b loop				#else, loops again
 		
-cont:	li $v0, 10
+cont:	li $v0, 1
+		lw $a0, num
+		syscall
+
+		li $v0, 10
 		syscall
 
 		
