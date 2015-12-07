ddd37b371a512102eca7478e2f4f3a751cd96cf0
Marco Tejedor González <marco.tejedorg@gmail.com>
2015-12-07 21:36:04 +0100

    Finished v4

--- atoi_4.asm
+++ atoi_4.asm
@@ -2,11 +2,12 @@
 	menen:		.asciiz	"Escriba una frase\n"
 	buffer:		.space	1024
 	saltolinea:	.asciiz	"\n"
+	menoverflow:	.asciiz "Se ha producido desbrdamiento\n"
 	
 .text
 
 main:
-		# Reads the sentence
+		# Asks for the data
 		li $v0, 4
 		la $a0, menen
 		syscall
@@ -33,13 +34,23 @@ loop:		lb $t1, 0($t0)
 		blt $t1, 48, cont		# LF is included
 		bgt $t1, 57, cont
 		mul $s0, $s0, 10
+		
+		# Version 4
+		mfhi $t3
+		bne $t3, 0, overflow		# hi is not 0
+		mflo $t4
+		blt $t4, 0, overflow		# hi is 0 but lo is negative
+		
 		subi $t2, $t1, 48
 		add $s0, $s0, $t2
+		
+		# Version 4
+		blt $s0, 0, overflow		# Shouldn't happen in this program
+		
 		addi $t0, $t0, 1
 		b loop				#else, loops again
 		
-cont:		
-		mul $s0, $s0, $s1
+cont:		mul $s0, $s0, $s1
 
 		# Print num
 		li $v0, 1
@@ -49,7 +60,7 @@ cont:
 		# Exit
 		li $v0, 10
 		syscall
-
 		
-		
-		
+overflow:	li $v0, 4
+		la $a0, menoverflow
+		syscall
